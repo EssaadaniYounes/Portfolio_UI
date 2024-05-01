af412f361cfd336dfa10a43e9cd620ac080cf62c
EssaadaniYounes <younesessaadani2003@gmail.com>
2024-05-01 18:00:57 +0200

    Remove tooltip and add modal

--- components/partials/ProjectTools.tsx
+++ components/partials/ProjectTools.tsx
@@ -1,31 +1,37 @@
 import React from "react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../ui/tooltip";
 import { CircleAlert } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 
 function ProjectTools({ technologies }: { technologies: string[] }) {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger className="ml-auto mr-4">
-          <CircleAlert size={25} className="ml-auto mr-4 cursor-pointer" />
-        </TooltipTrigger>
-        <TooltipContent className="capitalize">
-          <h2 className="text-lg font-medium text-gray-700 dark:text-gray-200">
-            Technologies :
-          </h2>
-          {technologies.map((tech) => (
-            <p key={tech} className="text-sm text-gray-400">
-              * {tech}
-            </p>
-          ))}
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Dialog>
+      <DialogTrigger className="ml-auto mr-4">
+        <CircleAlert size={25} className="ml-auto mr-4 cursor-pointer" />
+      </DialogTrigger>
+      <DialogContent className="capitalize">
+        <DialogHeader>
+          <DialogTitle>
+            <h2 className="text-lg font-medium text-gray-700 dark:text-gray-200">
+              Technologies :
+            </h2>
+          </DialogTitle>
+          <DialogDescription>
+            {technologies.map((tech) => (
+              <p key={tech} className="text-sm text-gray-400">
+                * {tech}
+              </p>
+            ))}
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
 
